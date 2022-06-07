f14e129a4cdda8e9c140769c3ad6725385317914
sentilianozefi <sentilianozefi5@gmail.com>
2022-06-07 11:04:48 +0200

    .

--- src/RecoverPage.js
+++ src/RecoverPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import {useState} from 'react';
+import { useState } from 'react';
 import "./PasswordRecovery.css";
 import "./styles.css";
 import "./Forms.css";
@@ -13,53 +13,75 @@ export default function Recover() {
     let params = useParams();
 
     const resetpass = () => {
-        fetch("http://192.168.70.108:8080/respas/"+params.token+"/"+newPassword)
-          .then(res =>{ if(!res.ok){
-              alert("Token expired or wrong token!")
-          }
-          else {res.json()
-          alert("Password saved!")}
-        })
-        }
-        
-      
+        fetch("http://192.168.70.108:8080/respas/" + params.token + "/" + newPassword)
+            .then(res => {
+                if (!res.ok) {
+                    alert("Token expired or wrong token!")
+                }
+                else {
+                    res.json()
+                    alert("Password saved!")
+                }
+            })
+    }
 
-      const reset = () =>{
-          if(newPassword === "" || confirmpass === ""){
-              alert("One or more of the required fields is empty!")
-          }
-          else if(newPassword !== confirmpass){
-              alert("Passwords do not match!")
-          }
-          else
-          resetpass();
-      }
-    
+
+
+    const reset = () => {
+        if (newPassword === "" || confirmpass === "") {
+            alert("One or more of the required fields is empty!")
+        }
+        else if (newPassword !== confirmpass) {
+            alert("Passwords do not match!")
+        }
+        else
+            resetpass();
+    }
+
 
     return (
 
         <div>
             <div className=" header">
-        <a href="/" className="home">Whistleblowing</a>
-        <div className="header-right">
-          <a className="login-btn" href="/login">Log in</a>
-          <a className="signup-btn" href="/signup">Sign up</a>
-        </div>
-      </div>
-      <div className="recover-body">
-      <div className="recover-area">
-      <div className="form-recover-password">
-            <div>
-                
-                <label>New password:</label>
-                <input type="password" value={newPassword} onChange={e=>setnewPassword(e.target.value)}/>
+                <a href="/" className="home">Whistleblowing</a>
+                <div className="header-right">
+                    <a className="login-btn" href="/login">Log in</a>
+                    <a className="signup-btn" href="/signup">Sign up</a>
+                </div>
             </div>
-            <div>
-                <label>Confirm password:</label>
-                <input type="password" value={confirmpass} onChange={e=>setconfirmpass(e.target.value)}/>
+            <div className="recover-body">
+
+                <div className="recover-area">
+
+                    <div className="form-recover-password">
+
+                        <div className='form-inputs-recover'>
+
+                            <label className='form-label'>New password:</label>
+
+                            <input className='form-input input-with-border' type="password" value={newPassword} onChange={e => setnewPassword(e.target.value)} />
+
+                        </div>
+
+                        <div className='form-inputs-recover'>
+
+                            <label className='form-label'>Confirm password:</label>
+
+                            <input className='form-input input-with-border' type="password" value={confirmpass} onChange={e => setconfirmpass(e.target.value)} />
+
+                        </div>
+
+                        <div className='button-save-pw'>
+
+                            <button className='signup-btn save-password' onClick={reset}>Save password</button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
-            <button onClick={reset}>Save password</button>
-            </div></div></div>
         </div>
     );
 }
