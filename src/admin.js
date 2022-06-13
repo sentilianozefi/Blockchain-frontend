d66a8445740c611d7f63767086c7ed0ee5f359ee
sentilianozefi <sentilianozefi5@gmail.com>
2022-06-13 18:01:01 +0200

    .

--- src/admin.js
+++ src/admin.js
@@ -1,13 +1,16 @@
+import {Link} from 'react-router-dom';
 import { useState } from "react";
 import "./styles.css";
 import "./Forms.css";
 import "./user.css";
 
 export default function Admin() {
-    const [admin, setadmin] = useState([]);
+    // const [admins, setadmins] = useState([]);
+    const [admin, setadmin] = useState("");
+    const [adminpass, setadminpass] = useState("");
     return (
         <div>
-            {admin === null &&
+            {/* {admins === null && */}
 
                 <div>
                     <div className=" header">
@@ -20,30 +23,31 @@ export default function Admin() {
                     <div className="form-content-2" >
                         <form onSubmit={e => e.preventDefault()} className="login-form">
 
-                            <h1 className="welcome">Log in</h1>
+                            <h1 className="welcome">Log in as administrator</h1>
                             <div className="form-inputs">
                                 <label className="form-label">Username: </label>
-                                <input className="form-input" type="text" value={inputValue} onChange={evt => setValue(evt.target.value)} placeholder="Enter your username"></input>
+                                <input className="form-input" type="text" value={admin} onChange={evt => setadmin(evt.target.value)} placeholder="Enter your username"></input>
                             </div>
 
                             <div className="form-inputs">
                                 <label className="form-label">Password: </label>
-                                <input className="form-input" type="password" value={passValue} onChange={evt => setPassValue(evt.target.value)} placeholder="Enter your password"></input>
+                                <input className="form-input" type="password" value={adminpass} onChange={evt => setadminpass(evt.target.value)} placeholder="Enter your password"></input>
                             </div>
 
-                            <button className="login-btn-2" onClick={login}>Log in</button>
-                            <div id="forgot-password">
+                            <button className="login-btn-2">Log in</button>
+                            {/* <div id="forgot-password">
                                 <Link to="/recover">Forgot password?</Link>
                             </div>
 
                             <p className="sign-up-bottom">
                                 Don't have an account yet? <br />
                                 <Link id="sign-up" to="/signup">Sign up here</Link>
-                            </p>
+                            </p> */}
 
                         </form>
                     </div>
-                </div>}
+                </div>
+                {/* } */}
         </div>
     )
 }
